abe6739fa9227cff9a76f1a443b0971db5d5888e
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-08-17 18:32:30 +0530

    Read or Write the address book using File IO

--- AddressBook.java
+++ AddressBook.java
@@ -1,161 +1,107 @@
-package com.AddressBook;
+package FileIO;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class AddressBook {
 
-	static Scanner sc = new Scanner(System.in);
-	static ArrayList<Contact> contactList = new ArrayList<Contact>();
+	private static Scanner sc = new Scanner(System.in);
+	private static File file = new File("AddressBook.txt");
+	static List<Contact> contact = new ArrayList<>();
 
-	public static void addContact() {
-		Contact contact = new Contact();
-
-		System.out.println("Enter the First Name : ");
-		String firstName = sc.next();
-		contact.setFirstName(firstName);
-
-		System.out.println("Enter the Last Name : ");
-		String lastName = sc.next();
-		contact.setLastName(lastName);
-
-		System.out.println("Enter the Address : ");
-		String address = sc.next();
-		contact.setAddress(address);
-
-		System.out.println("Enter the City : ");
-		String city = sc.next();
-		contact.setCity(city);
-
-		System.out.println("Enter the State : ");
-		String state = sc.next();
-		contact.setState(state);
-
-		System.out.println("Enter the ZIP Code : ");
-		long zipCode = sc.nextLong();
-		contact.setZip(zipCode);
-
-		System.out.println("Enter the Phone Number : ");
-		long phoneNumber = sc.nextLong();
-		contact.setPhoneNumber(phoneNumber);
-
-		System.out.println("Enter the E-mail: ");
-		String email = sc.next();
-		contact.setEmail(email);
-
-		contactList.add(contact);
-
+	public static void main(String[] args) throws IOException {
+		readPeopleFromFile();
+		showMainMenu();
 	}
 
-	public static void editContact() {
-		System.out.println("Enter first name that you want to Edit:");
-		String firstName = sc.next();
+	public static void addContact() throws IOException {
+		System.out.println("Enter Firstname: ");
+		String firstname = sc.nextLine();
+		System.out.println("Enter Lastname: ");
+		String lastname = sc.nextLine();
+		System.out.println("Enter City: ");
+		String city = sc.nextLine();
+		System.out.println("Enter State: ");
+		String state = sc.nextLine();
+		System.out.println("Enter ZipCode: ");
+		String zip = sc.nextLine();
+		System.out.println("Enter Phone Number: ");
+		String phoneNumber = sc.nextLine();
+		System.out.println("Enter Email Id: ");
+		String email = sc.nextLine();
 
-		for (int i = 0; i < contactList.size(); i++) {
-			if (contactList.get(i).getFirstName().equalsIgnoreCase(firstName)) {
-				Contact c = contactList.get(i);
-				System.out.print("Enter new First Name: ");
-				c.setFirstName(sc.next());
-				System.out.print("Enter  new Last Name : ");
-				c.setFirstName(sc.next());
-				System.out.print("Enter new City : ");
-				c.setCity(sc.next());
-				System.out.print("Enter new State : ");
-				c.setState(sc.next());
-				System.out.print("Enter new zip : ");
-				c.setZip(sc.nextLong());
-				System.out.print("Enter new PhoneNumber: ");
-				c.setPhoneNumber(sc.nextLong());
-				System.out.print("Enter new Email ID : ");
-				c.setEmail(sc.next());
-				contactList.set(i, c);
-				System.out.println("Edited Successfully!");
-			} else if (firstName != (contactList.get(i).getFirstName())) {
-				System.out.println("Not Found!");
-			}
+		Contact contactDetail = new Contact(firstname, lastname, city, state, zip, phoneNumber, email);
+		addToFile(contactDetail);
+		contact.add(contactDetail);
+		System.out.println("Added person: " + contactDetail);
+		System.out.println();
+		showMainMenu();
+	}
+
+	private static void addToFile(Contact contact) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+			writer.write(contact.toString());
+		} catch (IOException e) {
+			System.out.println(e);
 		}
 	}
 
-	public static void deleteContact() {
-		System.out.println("Enter first name that you want to Delete:");
-		String firstName = sc.next();
-		for (int i = 0; i < contactList.size(); i++) {
-			if (contactList.get(i).getFirstName().equalsIgnoreCase(firstName)) {
-				contactList.remove(i);
-				System.out.println("Deleted Successfully !");
-				break;
-			} else if (firstName != (contactList.get(i).getFirstName())) {
-				System.out.println("Not Found!");
+	private static boolean readPeopleFromFile() throws IOException {
+		List<Contact> contact = new ArrayList<Contact>();
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			String name = null;
+			while ((name = reader.readLine()) != null) {
+				Contact contact1 = new Contact(name, reader.readLine(), reader.readLine(), reader.readLine(),
+						reader.readLine(), name, name);
+				contact.add(contact1);
+				reader.readLine();
 			}
-		}
-	}
-
-	public static void displayAll() {
-		boolean is_Empty = contactList.isEmpty();
-		if (is_Empty == true)
-			System.out.println("Array List is Empty");
-		else
-			for (Contact c : contactList) {
-				System.out.println("Contact Details are :-\n" + c.getFirstName() + "\n" + c.getLastName() + "\n"
-						+ c.getAddress() + "\n" + c.getCity() + " \n" + c.getState() + " \n" + c.getPhoneNumber()
-						+ " \n" + c.getZip() + " \n" + c.getEmail());
-			}
-	}
-
-	public static boolean checkDuplicate(String f_name, List<Contact> contact) {
-		for (Contact c : contact) {
-			if (c.getFirstName().equals(f_name)) {
-				return true;
-			}
+			return true;
+		} catch (IOException e) {
+			System.out.println(e);
 		}
 		return false;
 	}
 
-	public static void searchByCityOrState(List<Contact> contact) {
-		System.out.println("Enter CityName: ");
-		String city = sc.next();
-		contact.stream().filter(c -> c.getCity().equals(city)).forEach(System.out::println);
+	public static void printData() throws IOException {
+		try {
+			Files.lines(new File("AddressBook.txt").toPath()).forEach(System.out::println);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	public static void viewPersonByCityOrState(List<Contact> contact) {
-		System.out.println("Enter CityName: ");
-		String city = sc.next();
-		contact.stream().filter(c -> c.getCity().equals(city)).forEach(
-				cn -> System.out.println("First Name : " + cn.getFirstName() + "  Last Name : " + cn.getFirstName()));
-	}
+	private static void showMainMenu() throws IOException {
+		System.out.println("1. Add person");
+		System.out.println("2. Show all contact details");
+		System.out.println("3. Exit from the program");
 
-	public static void countByCity(List<Contact> contact) {
-
-		System.out.println("Enter the name of the city:");
-		String city = sc.next();
-		Long countNamesByCity = contact.stream().filter(e -> city.equals(e.getCity())).count();
-		System.out.println(city + " : " + countNamesByCity);
-
-	}
-
-	public static void countByState(List<Contact> contact) {
-
-		System.out.println("Enter the name of the State:");
-		String state = sc.next();
-		Long countNamesByState = contact.stream().filter(e -> state.equals(e.getState())).count();
-		System.out.println(state + " : " + countNamesByState);
-	}
-
-	public static void sortedContactByFirstName(List<Contact> contact) {
-
-		List<Contact> sortedContact = contact.stream().sorted(new compareFirstName()).collect(Collectors.toList());
-		System.out.println(sortedContact);
-	}
-
-}
-
-class compareFirstName implements Comparator<Contact> {
-	@Override
-	public int compare(Contact o1, Contact o2) {
-
-		return o1.getFirstName().compareTo(o2.getFirstName());
+		String choice;
+		do {
+			choice = sc.nextLine();
+			switch (choice) {
+			case "1":
+				addContact();
+				break;
+			case "2":
+				printData();
+				showMainMenu();
+				break;
+			case "3":
+				System.out.println("Thank You..");
+				System.exit(0);
+				break;
+			default:
+				System.out.println("Enter numer from 1 to 3");
+			}
+		} while (!choice.equals("3"));
 	}
 }
--- Contact.java
+++ Contact.java
@@ -1,85 +1,51 @@
-package com.AddressBook;
+package FileIO;
 
 public class Contact {
-    String firstName, lastName, address, city, state, email;
-    long zip, phoneNumber;
 
-    public Contact() {
-    }
+	  private String firstName;
+	    private String lastName;
+	    private String city;
+	    private String state;
+	    private String zip;
+	    private String phoneNumber;
+	    private String email;
 
-    public Contact(String firstName, String lastName, String address, String city, String state, String email, long zip, long phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.email = email;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
+	    public Contact(String firstName, String lastName, String city, String state, String zip, String phoneNumber,
+	                   String email) {
+	        this.firstName = firstName;
+	        this.lastName = lastName;
+	        this.city = city;
+	        this.state = state;
+	        this.zip = zip;
+	        this.phoneNumber = phoneNumber;
+	        this.email = email;
+	    }
+	    public String getFirstName() {
+	        return firstName;
+	    }
+	    public String getLastName() {
+	        return lastName;
+	    }
+	    public String getCity() {
+	        return city;
+	    }
+	    public String getState() {
+	        return state;
+	    }
+	    public String getZip() {
+	        return zip;
+	    }
+	    public String getPhoneNumber() {
+	        return phoneNumber;
+	    }
+	    public String getEmail() {
+	        return email;
+	    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getZip() {
-        return zip;
-    }
-
-    public void setZip(long zip) {
-        this.zip = zip;
-    }
-
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-}
-
+	    @Override
+	    public String toString() {
+	        return "Contact [firstName=" + firstName + ", lastName=" + lastName + ", city=" + city + ", state=" + state
+	                + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]\n";
+	    }
+	}
