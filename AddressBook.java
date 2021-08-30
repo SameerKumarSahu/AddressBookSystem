52464db366e1e897477f7c69cfc6f9b00aec6351
Sameer Kumar Sahu <81921667+SameerKumarSahu@users.noreply.github.com>
2021-08-30 19:15:58 +0530

    Retrieve all the Entries from the DB

--- AddressBook.java
+++ AddressBook.java
@@ -1,84 +1,38 @@
-package com.AddressBook;
+package AddressBookService;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class AddressBook {
 
-	static Scanner sc = new Scanner(System.in);
-	static ArrayList<Contact> contactList = new ArrayList<Contact>();
+	public static void main(String[] args) {
+		try {
+			//Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbookservice", "root",
+					"Sameer@42");
+			Statement stm = con.createStatement();
+			String QUERY = "SELECT * FROM addressbook";
+			ResultSet rs = stm.executeQuery(QUERY);
 
-	public static void addContact() {
-		Contact contact = new Contact();
-
-		System.out.println("Enter the First Name : ");
-		String firstName = sc.next();
-		contact.setFirstName(firstName);
-
-		System.out.println("Enter the Last Name : ");
-		String lastName = sc.next();
-		contact.setLastName(lastName);
-
-		System.out.println("Enter the Address : ");
-		String address = sc.next();
-		contact.setAddress(address);
-
-		System.out.println("Enter the City : ");
-		String city = sc.next();
-		contact.setCity(city);
-
-		System.out.println("Enter the State : ");
-		String state = sc.next();
-		contact.setState(state);
-
-		System.out.println("Enter the ZIP Code : ");
-		long zipCode = sc.nextLong();
-		contact.setZip(zipCode);
-
-		System.out.println("Enter the Phone Number : ");
-		long phoneNumber = sc.nextLong();
-		contact.setPhoneNumber(phoneNumber);
-
-		System.out.println("Enter the E-mail: ");
-		String email = sc.next();
-		contact.setEmail(email);
-
-		contactList.add(contact);
-
-		for (Contact c : contactList) {
-			System.out.println("Contact Details are :-\n" + c.getFirstName() + "\n" + c.getLastName() + "\n "
-					+ c.getAddress() + "\n " + c.getCity() + " \n" + c.getState() + " \n" + c.getPhoneNumber() + " \n"
-					+ c.getZip() + " \n" + c.getEmail());
-		}
-	}
-
-	public static void editContact() {
-		System.out.println("Enter first name that you want to Edit:");
-		String firstName = sc.next();
-
-		for (int i = 0; i < contactList.size(); i++) {
-			if (contactList.get(i).getFirstName().equalsIgnoreCase(firstName)) {
-				Contact c = contactList.get(i);
-				System.out.print("Enter new First Name: ");
-				c.setFirstName(sc.next());
-				System.out.print("Enter  new Last Name : ");
-				c.setFirstName(sc.next());
-				System.out.print("Enter new City : ");
-				c.setCity(sc.next());
-				System.out.print("Enter new State : ");
-				c.setState(sc.next());
-				System.out.print("Enter new zip : ");
-				c.setZip(sc.nextLong());
-				System.out.print("Enter new PhoneNumber: ");
-				c.setPhoneNumber(sc.nextLong());
-				System.out.print("Enter new Email ID : ");
-				c.setEmail(sc.next());
-				contactList.set(i, c);
-				System.out.println("Edited Successfully!");
-			} else if (firstName != (contactList.get(i).getFirstName())) {
-				System.out.println("Not Found!");
+			// Display values
+			while (rs.next()) {
+				System.out.print("FirstName: " + rs.getString("FirstName"));
+				System.out.print("| LastName: " + rs.getString("LastName"));
+				System.out.print("| Address: " + rs.getString("Address"));
+				System.out.print("| City: " + rs.getString("City"));
+				System.out.print("| State: " + rs.getString("State"));
+				System.out.print("| Zip: " + rs.getInt("Zip"));
+				System.out.print("| PhoneNumber: " + rs.getString("PhoneNumber"));
+				System.out.print("| Email_ID: " + rs.getString("Email_ID"));
+				System.out.println("\n-----------------------------------------------------------");
+				
 			}
+		} catch (Exception e) {
+			System.out.println(e);
 		}
+
 	}
 
 }
